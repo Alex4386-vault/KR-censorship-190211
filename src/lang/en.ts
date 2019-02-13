18232e4421c447b8997301014aec102bf777bfa0
Joosung Park <park012241@park012241.me>
2019-02-13 14:33:27 +0900

    add : Korean and English

--- src/lang/en.ts
+++ src/lang/en.ts
@@ -1,12 +1,19 @@
-export const reqToBlocked = (blocked: string): string => `Requesting google.com with hostname modified to ${blocked}`;
+export const reqToBlocked = (blocked: string): string =>
+    `Requesting google.com with hostname modified to ${blocked}`;
 
-export const notCensored = (blocked: string): string => `${blocked} is OK.`;
+export const notCensored = (blocked: string): string =>
+    `${blocked} is OK.`;
 
-export const censored = (blocked: string): string => `${blocked} is being censored.`;
+export const censored = (blocked: string): string =>
+    `${blocked} is being censored.`;
 
-export const result = (isCensored: boolean): string => `\nResult :\n${isCensored ?
-    `Your network is currently CENSORED!\nPlease USE VPN to protect your privacy from government supervision!` :
-    `This network is not censored.\nEnjoy your internet life.`}`;
+export const result = (isCensored: boolean): string =>
+    `\nResult :\n` +
+    `${isCensored ?
+        `Your network is currently CENSORED!\n` +
+        `Please USE VPN to protect your privacy from government supervision!` :
+        `This network is not censored.\n` +
+        `Enjoy your internet life.`}`;
 
 export const error = (): string =>
     `This is something which should not happen or the censor logic was changed.\n` +
--- src/lang/ko.ts
+++ src/lang/ko.ts
@@ -1,12 +1,19 @@
-export const reqToBlocked = (blocked: string): string => `${blocked}에 요청중...`;
+export const reqToBlocked = (blocked: string): string =>
+    `${blocked}에 요청중...`;
 
-export const notCensored = (blocked: string): string => `${blocked}은(는) 검열되고 있지 않습니다`;
+export const notCensored = (blocked: string): string =>
+    `${blocked}은(는) 검열되고 있지 않습니다`;
 
-export const censored = (blocked: string): string => `${blocked}은(는) 검열되고 있습니다`;
+export const censored = (blocked: string): string =>
+    `${blocked}은(는) 검열되고 있습니다`;
 
-export const result = (isCensored: boolean): string => `\n결과 :\n${isCensored ?
-    `이 네트워크는 검열 되어있습니다!\n프라이버시 보호를 위해 VPN 사용을 적극 권장합니다!` :
-    `이 네트워크는 검열 되어있지 않습니다!\n자유로운 인터넷을 즐기세요!`}`;
+export const result = (isCensored: boolean): string =>
+    `\n결과 :\n` +
+    `${isCensored ?
+        `이 네트워크는 검열 되어있습니다!\n` +
+        `프라이버시 보호를 위해 VPN 사용을 적극 권장합니다!` :
+        `이 네트워크는 검열 되어있지 않습니다!\n` +
+        `자유로운 인터넷을 즐기세요!`}`;
 
 export const error = (): string =>
     `예상하지 못한 오류가 발생하였습니다.\n` +
